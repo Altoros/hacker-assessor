8242d068005cef9923d4cc55734de10595b9fb71
Eloy Espinaco <eloyesp@gmail.com>
2015-12-09 11:12:03 -0300

    Make inspect usefull for requirements.

--- app/models/requirement.rb
+++ app/models/requirement.rb
@@ -10,4 +10,12 @@ class Requirement < ActiveRecord::Base
   def seniority
     Seniority.new(super)
   end
+
+  def inspect full = false
+    return super if full
+    "#<Requirement##{ id }, experience: #{ experience.inspect }," +
+      " seniority: #{ seniority }, career_id: #{ career.name }>"
+  rescue
+    super
+  end
 end
