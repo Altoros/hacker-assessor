c795c0f98e19eefcee394c0d8cb84471246f0eac
Eloy Espinaco <eloyesp@gmail.com>
2015-12-15 13:41:55 -0300

    Add more test.

--- test/presentations/dashboard_test.rb
+++ test/presentations/dashboard_test.rb
@@ -2,8 +2,12 @@ require 'test_helper'
 
 class DashboardTest < ActiveSupport::TestCase
 
-  def dashboard
-    @dashboard ||= Dashboard.new hackers(:jorge)
+  def dashboard hacker = hackers(:jorge)
+    @dashboard ||= Dashboard.new hacker
+  end
+
+  def experience level: 3, skill: skills(:tdd)
+    Experience.new skill, level
   end
 
   test 'have a title' do
@@ -21,9 +25,14 @@ class DashboardTest < ActiveSupport::TestCase
   test 'shows matching requirements' do
     matching_requirements = dashboard.matching_requirements
     assert matching_requirements
-    assert matching_requirements.first.required
-    assert matching_requirements.first.skill_name
-    assert matching_requirements.first.current
+    assert_equal experience, matching_requirements.first.required
+    assert_equal 'tdd', matching_requirements.first.skill_name
+    assert_equal experience(level: 2), matching_requirements.first.current
+    assert_equal 1, matching_requirements.size
+  end
+
+  test 'find all the missign requirements' do
+    assert_equal 2, dashboard(hackers(:rodrigo)).matching_requirements.size
   end
 
   test 'elder hackers can edit the dashboard' do
